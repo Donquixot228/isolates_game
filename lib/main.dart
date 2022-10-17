a4217a37335faede08c48896f228a3e4e1350bd6
Donquixot228 <kirilnoskov402@gmail.com>
2022-10-17 15:47:58 +0300

    first simple isolate example

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,146 @@
+import 'dart:isolate';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:isolates/utilits/global_vars.dart';
 
-import 'game_core/game.dart';
+// import 'package:flutter/services.dart';
+// import 'package:isolates/utilits/global_vars.dart';
+//
+// import 'game_core/game.dart';
+void main() => runApp(const MyApp());
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .whenComplete(() {
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.manual,
-      overlays: [SystemUiOverlay.bottom],
-    );
+void isolateFunc(int finalNum) {
+  int num = 0;
+  while (num < finalNum) {
+    num++;
+    if((num % 100) == 0) {
+      print('Isolate = $num');
+    }
+  }
+}
 
-    runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SafeArea(
-          child: Scaffold(
-            body: MyApp(),
-          ),
-        ),
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(
+        title: 'Flutter Demo Home Page',
       ),
     );
-  });
+  }
 }
 
-class MyApp extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
   @override
-  _MyAppState createState() => _MyAppState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
   @override
-  void didChangeDependencies() {
-    initGame(context);
-    super.didChangeDependencies();
+  void initState() {
+    Isolate.spawn(isolateFunc, 1000);
+    super.initState();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/background.png"),
-          fit: BoxFit.fill,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
-      child: Game(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
-
-  void initGame(BuildContext context) {
-    GlobalVars.screenWidth = MediaQuery.of(context).size.width;
-    GlobalVars.screenHeight = MediaQuery.of(context).size.height;
-  }
 }
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setPreferredOrientations(
+//           [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+//       .whenComplete(() {
+//     SystemChrome.setEnabledSystemUIMode(
+//       SystemUiMode.manual,
+//       overlays: [SystemUiOverlay.bottom],
+//     );
+//
+//     runApp(
+//       MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: SafeArea(
+//           child: Scaffold(
+//             body: MyApp(),
+//           ),
+//         ),
+//       ),
+//     );
+//   });
+// }
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void didChangeDependencies() {
+//     initGame(context);
+//     super.didChangeDependencies();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         image: DecorationImage(
+//           image: AssetImage("assets/background.png"),
+//           fit: BoxFit.fill,
+//         ),
+//       ),
+//       child: Game(),
+//     );
+//   }
+//
+//   void initGame(BuildContext context) {
+//     GlobalVars.screenWidth = MediaQuery.of(context).size.width;
+//     GlobalVars.screenHeight = MediaQuery.of(context).size.height;
+//   }
+// }
