e9c53c93539225832a5ccc13cf124eac10ec34c4
Donquixot228 <kirilnoskov402@gmail.com>
2022-10-18 10:29:25 +0300

    start working

--- lib/main.dart
+++ lib/main.dart
@@ -4,163 +4,167 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-// import 'package:flutter/services.dart';
-// import 'package:isolates/utilits/global_vars.dart';
+import 'package:flutter/services.dart';
+import 'package:isolates/utilits/global_vars.dart';
+
+import 'game_core/game.dart';
+
+// void main() => runApp(const MyApp());
 //
-// import 'game_core/game.dart';
-void main() => runApp(const MyApp());
-
-void isolateFunc(int finalNum) {
-  int num = 0;
-  while (num < finalNum) {
-    num++;
-    if ((num % 100) == 0) {
-      print('Isolate = $num');
-    }
-  }
-}
-
-int computeFunc(int finalNum) {
-  int num = 0;
-  while (num < finalNum) {
-    num++;
-    if ((num % 100) == 0) {
-      print('Compute = $num');
-    }
-  }
-  return num;
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(
-        title: 'Flutter Demo Home Page',
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    Isolate.spawn(isolateFunc, 1000);
-    super.initState();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  Future<void> runCompute() async {
-    _counter = await compute(computeFunc, 2000);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              child: const Text('Isolate'),
-              onPressed: () async {
-                await runCompute();
-              },
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   SystemChrome.setPreferredOrientations(
-//           [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-//       .whenComplete(() {
-//     SystemChrome.setEnabledSystemUIMode(
-//       SystemUiMode.manual,
-//       overlays: [SystemUiOverlay.bottom],
-//     );
+// // void isolateFunc(int finalNum) {
+// //   int num = 0;
+// //   while (num < finalNum) {
+// //     num++;
+// //     if ((num % 100) == 0) {
+// //       print('Isolate = $num');
+// //     }
+// //   }
+// // }
 //
-//     runApp(
-//       MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: SafeArea(
-//           child: Scaffold(
-//             body: MyApp(),
-//           ),
-//         ),
-//       ),
-//     );
-//   });
-// }
+// // int computeFunc(int finalNum) {
+// //   int num = 0;
+// //   while (num < finalNum) {
+// //     num++;
+// //     if ((num % 100) == 0) {
+// //       print('Compute = $num');
+// //     }
+// //   }
+// //   return num;
+// // }
 //
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   @override
-//   void didChangeDependencies() {
-//     initGame(context);
-//     super.didChangeDependencies();
-//   }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 //
 //   @override
 //   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: const BoxDecoration(
-//         image: DecorationImage(
-//           image: AssetImage("assets/background.png"),
-//           fit: BoxFit.fill,
-//         ),
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
 //       ),
-//       child: Game(),
+//       home: const MyHomePage(
+//         title: 'Flutter Demo Home Page',
+//       ),
 //     );
 //   }
-//
-//   void initGame(BuildContext context) {
-//     GlobalVars.screenWidth = MediaQuery.of(context).size.width;
-//     GlobalVars.screenHeight = MediaQuery.of(context).size.height;
-//   }
 // }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   // @override
+//   // void initState() {
+//   //   Isolate.spawn(isolateFunc, 1000);
+//   //   super.initState();
+//   // }
+//
+//   // void _incrementCounter() {
+//   //   setState(() {
+//   //     _counter++;
+//   //   });
+//   // }
+//   //
+//   // Future<void> runCompute() async {
+//   //   _counter = await compute(computeFunc, 2000);
+//   //   setState(() {});
+//   // }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             ElevatedButton(
+//               child: const Text('Isolate'),
+//               onPressed: () async {
+//                 await runCompute();
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+//}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(
+    () {
+      SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom],
+      );
+
+      runApp(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SafeArea(
+            child: Scaffold(
+              body: MyApp(),
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void didChangeDependencies() {
+    initGame(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/background.png"),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Game(),
+    );
+  }
+
+  void initGame(BuildContext context) {
+    GlobalVars.screenWidth = MediaQuery.of(context).size.width;
+    GlobalVars.screenHeight = MediaQuery.of(context).size.height;
+  }
+}
